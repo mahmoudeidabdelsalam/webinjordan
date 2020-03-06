f5c55dd74e41fa752cdd4339f5ffe4a2d3047dcf
Mahmoud <mahmoud@eramint.com>
2020-03-07 01:33:56 +0200

    Make some field automatic filled based in data from the previous visit or filed

--- view/doctor/add_new_visit.php
+++ view/doctor/add_new_visit.php
@@ -1,33 +1,56 @@
 <?php if ($_GET['add_new_visit']): ?>
-<style type="text/css">
-    .wrap-content {
-        /*margin-left: 300px;*/
-        float: none;
-        /*padding: 50px 40px;*/
-        /*transition: all .3s;*/
+  <style type="text/css">
+      .wrap-content {
+          float: none;
+      }
+  </style>
+
+  <?php 
+    $patient_id   = $_GET['add_new_visit'];
+
+    $vist_data = get_field('visits', $patient_id);
+
+    $last_visit =  end($vist_data);
+
+    $post = get_post($last_visit);
+
+    $fields = get_field_objects($post->ID);
+
+    $array = [];
+
+    foreach($fields as $field) {
+      $Name = $field['name'];
+      $Key = $field['key'];
+      $array[$Key] = get_field($Name, $post->ID);
     }
-</style>
-	<div class="content-top col-xs-12">
-		<div class="logo-co">
-			<img src="<?php bloginfo('template_directory'); ?>/images/logo.png" alt="">
-			<div class="co-head">
-				<h3>
-					new
-					<span> Visit</span>
-				</h3>
-			</div>
-		</div>
-		<div class="co-extra">
-			<a href="<?php home_url('admin/?reports=1'); ?>" class="btn">reports</a>
-		</div>
-	</div>
-	<div class="case-head col-xs-12">
-		<h3>Visit  Details</h3>
 
-	</div>
+    $args = array(
+      'values' => $array,
+    );
+  ?>
 
-<?php echo  do_shortcode('[advanced_form form="form_5df69132505ce"]'); ?>
- 
+    
+
+    <div class="content-top col-xs-12">
+      <div class="logo-co">
+        <img src="<?php bloginfo('template_directory'); ?>/images/logo.png" alt="">
+        <div class="co-head">
+          <h3>
+            new
+            <span> Visit</span>
+          </h3>
+        </div>
+      </div>
+      <div class="co-extra">
+        <a href="<?php home_url('admin/?reports=1'); ?>" class="btn">reports</a>
+      </div>
+    </div>
+    <div class="case-head col-xs-12">
+      <h3>Visit  Details</h3>
+
+    </div>
+
+  <?php advanced_form("form_5df69132505ce", $args); ?>
 
  <?php endif; ?>
 <script type="text/javascript"> 
